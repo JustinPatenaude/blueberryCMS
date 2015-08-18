e4d7dc02cb3ee3d69e40a5ac6e7f45abf01663bd
Justin Patenaude <me@justinpatenaude.com>
2015-08-18 18:06:10 -0500

    Testing

--- js/admin.js
+++ js/admin.js
@@ -1,18 +1,34 @@
 $(function() {
 
-var userName = "JustinPatenaude";
+var userName = "";
+var oauthToken = "";
 
 $('.github_login').click(function(){
   window.open('https://github.com/login/oauth/authorize?client_id=e4a530296caca9a0979d&state=fhf74hfgdje8hfgfjhdg4627');
 });
 
-var github = new Github({
-  username: "JUSTINPATENAUDE",
-  password: "",
-  auth: "basic"
+window.addEventListener('message', function (event) {
+	var code = event.data;
+	$.get('http://blueberry.justinpatenaude.com/token.php?code=' + code, function (access_token) {
+    oauthToken = access_token;
+		$.getJSON('https://api.github.com/user?access_token=' + access_token, function (user) {
+			userName = user.login;
+		});
+	});
 });
 
-var repo = github.getRepo(userName, 'jekyll');
+var github = new Github({
+  token: oauthToken,
+  auth: "oauth"
+});
+
+var user = github.getUser();
+
+user.repos(function(err, repos) {
+  repos.append('.admin');
+});
+
+/*var repo = github.getRepo(userName, 'jekyll');
 
 repo.read('gh-pages', 'index.html', function(err, data) {
   $('.show_page').html(data);
@@ -21,8 +37,6 @@ repo.read('gh-pages', 'index.html', function(err, data) {
 $('.save_page').click(function(){
   var newContents = $('.show_page').val();
   repo.write('gh-pages', 'index.html', newContents, 'Updated from website', function(err) {});
-});
-
-$()
+});*/
 
 });
